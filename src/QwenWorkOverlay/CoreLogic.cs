872791f6532a252c7bc9a4504ece9acf7714ca7f
Anton Gameza <antony502189@gmail.com>
2026-08-12 02:16:07 +0300

    Extract testable native window style policy

--- src/QwenWorkOverlay/CoreLogic.cs
+++ src/QwenWorkOverlay/CoreLogic.cs
@@ -6,7 +6,7 @@ public static class VirtualMixOutputPolicy
         !string.IsNullOrWhiteSpace(name) &&
         System.Text.RegularExpressions.Regex.IsMatch(
             name,
-            "virtual|cable|voicemeeter|loopback|blackhole",
+            "virtual|cable|voicemeeter|loopback|blackhole|vb-audio",
             System.Text.RegularExpressions.RegexOptions.IgnoreCase);
 }
 
@@ -21,6 +21,7 @@ public static class ClipboardPolicy
 public sealed class RightCtrlStateMachine
 {
     public bool IsDown { get; private set; }
+
     public bool OnDown()
     {
         if (IsDown) return false;
@@ -40,4 +41,20 @@ public static class NativeCapturePrivacyPolicy
 {
     // SetWindowDisplayAffinity only applies safely when the top-level target window belongs to the calling process.
     public static bool CanApplyDirectly(int controllerProcessId, int targetProcessId) => controllerProcessId == targetProcessId;
+}
+
+public static class WindowStylePolicy
+{
+    public static bool NeedsLayeredWindow(double opacity, bool clickThrough) => opacity < .999 || clickThrough;
+
+    public static long ComputeExtendedStyle(long originalStyle, double opacity, bool clickThrough)
+    {
+        var desired = originalStyle;
+        if (NeedsLayeredWindow(opacity, clickThrough)) desired |= Native.WS_EX_LAYERED;
+        else if ((originalStyle & Native.WS_EX_LAYERED) == 0) desired &= ~Native.WS_EX_LAYERED;
+
+        if (clickThrough) desired |= Native.WS_EX_TRANSPARENT;
+        else if ((originalStyle & Native.WS_EX_TRANSPARENT) == 0) desired &= ~Native.WS_EX_TRANSPARENT;
+        return desired;
+    }
 }
